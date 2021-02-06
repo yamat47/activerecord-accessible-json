9e865947fd31c2fbb74712d346fad24edf30c348
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2021-02-06 20:02:02 +0900

    Exec: bundle exec rubocop -c .rubocop.yml --only Layout/EmptyLinesAroundBlockBody -A

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -13,12 +13,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_01_26_150045) do
-
   create_table 'posts', force: :cascade do |t|
     t.json 'author', null: false
     t.json 'tags', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
